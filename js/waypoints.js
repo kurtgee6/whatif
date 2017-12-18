41431242307549d7de1396ee0d9de106d8ce31b0
kurtgee6 <kurtgee11@gmail.com>
2017-12-17 21:22:22 -0500

    initial files

---
+++ js/waypoints.js
@@ -0,0 +1,19 @@
+var $para = $('.para');
+var $word = $('.word');
+
+$para.waypoint(function (direction){
+    if(direction == 'down') {
+        $para.addClass('js-para-animate');
+    } else {
+        $para.removeClass('js-para-animate');        
+    }
+}, {offset: '60px'})
+
+$word.waypoint(function (direction){
+    if(direction == 'down') {
+        $word.addClass('js-word-animate');
+        console.log('fsadf');
+    } else {
+        $word.removeClass('js-word-animate');        
+    }
+}, {offset: '250px'})
