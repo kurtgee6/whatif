0a52cdf2ebcbdfe84cfc39dc98f28990b2eedc0d
kurtgee6 <kurtgee11@gmail.com>
2017-12-17 21:56:14 -0500

    initial files

--- js/waypoints.js
+++ js/waypoints.js
@@ -1,19 +1,25 @@
-var $para = $('.para');
-var $word = $('.word');
 
-$para.waypoint(function (direction){
-    if(direction == 'down') {
-        $para.addClass('js-para-animate');
-    } else {
-        $para.removeClass('js-para-animate');        
-    }
-}, {offset: '60px'})
+ $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
 
-$word.waypoint(function (direction){
-    if(direction == 'down') {
-        $word.addClass('js-word-animate');
-        console.log('fsadf');
-    } else {
-        $word.removeClass('js-word-animate');        
-    }
-}, {offset: '250px'})
+    var $para = $('.para');
+    var $word = $('.word');
+    
+    $para.waypoint(function (direction){
+        if(direction == 'down') {
+            $para.addClass('js-para-animate');
+        } else {
+            $para.removeClass('js-para-animate');        
+        }
+    }, {offset: '60px'})
+    
+    $word.waypoint(function (direction){
+        if(direction == 'down') {
+            $word.addClass('js-word-animate');
+            console.log('fsadf');
+        } else {
+            $word.removeClass('js-word-animate');        
+        }
+    }, {offset: '450px'})
+
+  });
